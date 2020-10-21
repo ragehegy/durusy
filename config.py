678136ba96e7efb19c6dc90dc55fbbf8dcfef329
ragehegy <mohamed.rageh.qaid@gmail.com>
2020-10-21 14:36:42 +0200

    auth editted

--- config.py
+++ config.py
@@ -5,14 +5,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'mail.office365.com')
+    # MAIL_SERVER = os.environ.get('MAIL_SERVER', 'mail.office365.com')
+    # MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = "mhmdrgh@gmail.com"
+    MAIL_PASSWORD = "M7medRag7"
     
     FLASKY_MAIL_SUBJECT_PREFIX = '[Durusy Online]'
-    FLASKY_MAIL_SENDER = 'Mohamed Rageh <mohamed.rageh@durusy.online>'
+    FLASKY_MAIL_SENDER = 'Mohamed Rageh <mhmdrgh@gmail.com>'
     # FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SSL_REDIRECT = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
